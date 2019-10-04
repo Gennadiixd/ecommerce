3e6230c23b560f25893178a3b78fda14788cd9b7
gennadii <gennadiixd@gmail.com>
2019-10-04 19:06:58 +0300

    added TODO

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes'
+// TODO: rectify volnurability - in jwt token 
+// can be easily modify from frontend !!!
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
--- client/src/user/adminDashboard.js
+++ client/src/user/adminDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../core/layout';
 import {isAuthenticated} from '../auth';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 export default function AdminDashboard() {
 
