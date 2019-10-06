0848bfdcac61bf8ca6224bff62e9a4915404bf38
gennadii <gennadiixd@gmail.com>
2019-10-07 01:10:36 +0300

    working with addProduct form

--- client/src/admin/addProduct.js
+++ client/src/admin/addProduct.js
@@ -5,77 +5,169 @@ import {Link} from 'react-router-dom';
 import {createProduct} from './apiAdmin'
 
 export default function AddProduct() {
-    const [name, setName] = useState('');
-    const [error, setError] = useState(false);
-    const [success, setSuccess] = useState(false);
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: false,
+        error: false,
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    });
+
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData,
+    } = values
+    //componentDidMount analog
+    useEffect(() => {
+        setValues({
+            ...values,
+            formData: new FormData()
+        })
+    }, [])
 
     const {user, token} = isAuthenticated();
 
-    const handleChange = (event) => {
-        setError(false);
-        setSuccess(false);
-        setName(event.target.value);
+    const handleChange = (name) => (event) => {
+        const value = name === 'photo'
+            ? event.target.files[0]
+            : event.target.value
+
+        formData.set(name, value)
+        setValues({
+            ...values,
+            error: false,
+            [name]: value,
+        })
     }
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     setError(false);
-    //     setSuccess(false);
-    //     createCategory(user._id, token, name)
-    //         .then(data => {
-    //             if (data.error) {
-    //                 setError(true);
-    //             } else {
-    //                 setError(false);
-    //                 setSuccess(true);
-    //             }
-    //         })
-    // }
+    const newPostForm = () => (
+        <form className="mb-3">
+            <h4>Post Photo</h4>
+            <div className="form-group">
+                <label className="btn btn-secondary">
+                    <input
+                        onChange={handleChange('photo')}
+                        type="file"
+                        name="photo"
+                        accept="image/*"
+                    />
+                </label>
+            </div>
 
-    const showSuccess = () => {
-        if (success) {
-            return (
-                <h3 className="text-success">{name} is successfully created</h3>
-            )
-        }
-    }
-    const showError = () => {
-        if (error) {
-            return (
-                <h3 className="text-danger">
-                    {name} already existis,<br /> shuld be unique
-                </h3>
-            )
-        }
-    }
+            <div className="form-group">
+                <label
+                    className="text-muted"
+                >
+                    Name
+                </label>
+                <input
+                    onChange={handleChange('name')}
+                    type="text"
+                    className="form-control"
+                    value={name}
+                />
+            </div>
 
-    const goBack = () => (
-        <div className="mt-5">
-            <Link to="/admin/dashboard" className="text-warning">
-                Back to Dashboard
-            </Link>
-        </div>
+            <div className="form-group">
+                <label
+                    className="text-muted"
+                >
+                    Description
+                </label>
+                <textarea
+                    onChange={handleChange('description')}
+                    type="text"
+                    className="form-control"
+                    value={description}
+                />
+            </div>
+
+            <div className="form-group">
+                <label
+                    className="text-muted"
+                >
+                    Price
+                </label>
+                <textarea
+                    onChange={handleChange('price')}
+                    type="number"
+                    className="form-control"
+                    value={price}
+                />
+            </div>
+
+            <div className="form-group">
+                <label
+                    className="text-muted"
+                >
+                    Category
+                </label>
+                <select
+                    onChange={handleChange('category')}
+                    className="form-control"
+                >
+                    <option value="Node">Node</option>
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label
+                    className="text-muted"
+                >
+                    Quantity
+                </label>
+                <input
+                    onChange={handleChange('quantity')}
+                    type="number"
+                    className="form-control"
+                    value={quantity}
+                />
+            </div>
+
+            <div className="form-group">
+                <label
+                    className="text-muted"
+                >
+                    Shipping
+                </label>
+                <select
+                    onChange={handleChange('shipping')}
+                    className="form-control"
+                >
+                    <option value="0">
+                        No
+                    </option>
+                    <option value="1">
+                        Yes
+                    </option>
+                </select>
+            </div>
+
+            <button
+                className="btn btn-outline-primary"
+            >
+                Create product
+            </button>
+        </form>
     )
-
-    // const newCategoryForm = () => (
-    //     <form
-    //         onSubmit={handleSubmit}
-    //     >
-    //         <div className="form-group">
-    //             <label className="text-muted">Name</label>
-    //             <input
-    //                 type="text"
-    //                 className="form-control"
-    //                 onChange={handleChange}
-    //                 value={name}
-    //                 autoFocus
-    //             />
-    //         </div>
-    //         <button className="btn btn-outline-primary">
-    //             Create Category
-    //         </button>
-    //     </form>
-    // );
 
     return (
         <Layout
@@ -84,7 +176,7 @@ export default function AddProduct() {
         >
             <div className="row">
                 <div className="col-md-8 offset-md-2">
-                    ...
+                    {newPostForm()}
                 </div>
             </div>
         </Layout>
